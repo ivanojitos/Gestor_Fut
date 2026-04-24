4ae8f6f0bc9bb69337b4b090eadcad3ad75c6fdb
Ivan Alvarez Valencia (el pari) <ivan.alvarez@servimsa.com.mx>
2026-04-24 07:50:16 -0600

    se configuro a node

--- src/views/Session/createUsuarioView.vue
+++ src/views/Session/createUsuarioView.vue
@@ -138,7 +138,7 @@ const guardarUsuario = async () => {
       }
     });
 
-    const response = await axios.post("https://backgestorfut-api-gdbufbgnecdbcqdm.westeurope-01.azurewebsites.net/api/createJugador", data);
+    const response = await axios.post("https://back-node-gestor-fut-hbggakfghgaqe3cs.westeurope-01.azurewebsites.net/api/createJugador", data);
 
     if (response.data.ok) {
       alert(response.data.message);
