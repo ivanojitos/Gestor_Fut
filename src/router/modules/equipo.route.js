01bd43fe99b09b36a1182bb1f93e86b696339ebd
Ivan Alvarez Valencia (el pari) <ivan.alvarez@servimsa.com.mx>
2026-05-03 20:33:50 -0600

    se agrego la logica para modo juego

--- src/router/modules/equipo.route.js
+++ src/router/modules/equipo.route.js
@@ -3,5 +3,10 @@ export default [
     path: "/equipoDetalle",
     name: "EquipoDetalle",
     component: () => import("@/views/Dashboard/Perfil/EquipoDetalleView.vue"),
+  },
+    {
+    path: "/modoJuego",
+    name: "ModoJuego",
+    component: () => import("@/views/Dashboard/Perfil/ModoJuegoView.vue"),
   }
 ];
